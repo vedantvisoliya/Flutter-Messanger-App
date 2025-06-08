b162fa80db4077e25cfb7a47b5bdefdede2e20ff
Vedant Kumar <vedantvisoliya@gmail.com>
2025-06-09 00:18:45 +0530

    coded profile page, project is completed, and maybe some features can be added in future

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,23 +1,92 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:message_app/services/auth/auth_services.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
   @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  final AuthServices authServices = AuthServices();
+
+  // name
+  String? name;
+
+  Future<void> fetchUserName() async {
+    String userName = await authServices.getCurrentUserName();
+    setState(() {
+      name = userName;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUserName();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    if (name == null) {
+      return Center(child: CircularProgressIndicator());
+    }
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         title: Text(
           "P R O F I L E",
-          style: GoogleFonts.poppins(
-            fontWeight: FontWeight.w500,
-            fontSize: 19,
-          ),
+          style: GoogleFonts.poppins(fontWeight: FontWeight.w500, fontSize: 19),
         ),
         centerTitle: true,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(45.0),
+                  border: Border.all(
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                    width: 2,
+                  ),
+                ),
+                child: Icon(
+                  Icons.person,
+                  color: Theme.of(context).colorScheme.primary,
+                  size: 56,
+                ),
+              ),
+
+              const SizedBox(height: 20.0),
+
+              Text(
+                name!,
+                style: GoogleFonts.montserrat(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+
+              const SizedBox(height: 10.0),
+
+              Text(
+                authServices.getCurrentUser()!.email!,
+                style: GoogleFonts.montserrat(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
